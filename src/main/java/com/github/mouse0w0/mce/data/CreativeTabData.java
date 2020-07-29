92cd7a893cf5195c483a26a856652ec86bc33164
Mouse <mouse0w0@qq.com>
2020-07-29 20:41:14 +0800

    Rename to CreativeTabData

--- src/main/java/com/github/mouse0w0/mce/data/CreativeTabData.java
+++ src/main/java/com/github/mouse0w0/mce/data/CreativeTabData.java
@@ -1,12 +1,12 @@
 package com.github.mouse0w0.mce.data;
 
-public class CreativeTab {
+public class CreativeTabData {
 
     private String id;
     private String translationKey;
     private Item item;
 
-    public CreativeTab(String id, String translationKey, Item item) {
+    public CreativeTabData(String id, String translationKey, Item item) {
         this.id = id;
         this.translationKey = translationKey;
         this.item = item;
