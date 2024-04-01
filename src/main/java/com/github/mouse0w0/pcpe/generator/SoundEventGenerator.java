7b9193a78d6d7f5e71b9787de06584093034fd5f
Mouse <mouse0w0@qq.com>
2024-04-02 00:07:53 +0800

    Update SoundEventGenerator

--- src/main/java/com/github/mouse0w0/pcpe/generator/SoundEventGenerator.java
+++ src/main/java/com/github/mouse0w0/pcpe/generator/SoundEventGenerator.java
@@ -33,11 +33,24 @@ public class SoundEventGenerator implements DataGenerator {
     @Override
     public void exportTranslation(Exporter exporter, Map<String, String> translations) {
         for (SoundEvent soundEvent : data) {
-            String translationKey = "subtitles." + getRegistryName(soundEvent).getPath();
+            String registryPath = getRegistryName(soundEvent).getPath();
+            String translationKey = "subtitles." + registryPath;
             if (I18n.hasKey(translationKey)) {
                 translations.put(getTranslationKey(soundEvent), I18n.format(translationKey));
+            } else {
+                int lastIndex = registryPath.lastIndexOf('.');
+                int secondLastIndex = registryPath.lastIndexOf('.', lastIndex - 1);
+                String objectName = capitalize(registryPath.substring(secondLastIndex + 1, lastIndex).replace('_', ' '));
+                String behaviorName = registryPath.substring(lastIndex + 1).replace('_', ' ');
+                translations.put(getTranslationKey(soundEvent), objectName + " " + behaviorName);
             }
         }
+    }
+
+    private static String capitalize(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+        return sb.toString();
     }
 
     private static ResourceLocation getRegistryName(SoundEvent soundEvent) {
